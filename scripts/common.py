d8131c8b69c543c518c541aa8ae3950a559e46d7
Baraa <bmsaqr@gmail.com>
2026-07-28 12:14:32 +0200

    tomllib issue

--- scripts/common.py
+++ scripts/common.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
 import os
-import tomllib
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
 from pathlib import Path
 from typing import Iterable, Mapping, Sequence
 
